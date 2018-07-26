a1cff8d09920711caa6dc78f945ea38cc77eee3a
swarupareddyperam <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-26 15:38:56 +0530

    Pos1.java

---
+++ Pos1.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Pos1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a value");
+		n=sc.nextInt();
+		if(n>0)
+		System.out.println("positive number");
+		else
+		System.out.println("Negative");
+		
+	}
+}
+
+
+
+
