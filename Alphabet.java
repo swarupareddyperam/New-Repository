1ded82e53c0f190fffa8f4f7f52f1a53046748f3
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-26 19:25:43 +0530

    Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a value");
+		char ch=sc.next().charAt(0);
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Alphabet");
+		else
+		System.out.println("Not a Alphabet");
+		
+		
+	}
+}
