9107018a0fc52b201836d0b7aed0491d8adffd28
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 09:52:18 +0530

    SumNatural,java

---
+++ SumNatural.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+import java.lang.*;
+				
+import java.io.*;
+				
+				
+/* Name of the class has to be "Main" only if the class is public. */
+				
+class SumNatural				
+{
+					
+public static void main (String[] args) throws java.lang.Exception
+					
+{
+						
+int n,sum=0;
+						
+Scanner sc=new Scanner(System.in);
+                        
+System.out.println("Enter n value");
+                        
+n=sc.nextInt();
+  
+for(int i=1;i<=n;++i)
+{
+sum=sum+i;
+}                      
+                       
+                      
+System.out.println("sum"+sum);
+
+					
+}
+				
+}
