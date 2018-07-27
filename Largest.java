eee73e1be76ca5571de084e8856eeb2317db3be7
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 09:31:14 +0530

    Largest.java

---
+++ Largest.java
@@ -0,0 +1,47 @@
+/* package whatever; // don't place package name! */
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Largest{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+		
+int a,b,c;
+		
+Scanner sc=new Scanner(System.in);
+		
+System.out.println("Enter a,b,c");
+		
+a=sc.nextInt();
+		
+b=sc.nextInt();
+		
+c=sc.nextInt();
+		
+if(a>b)
+		
+System.out.println("a is greater");
+		
+else if(b>c)
+		
+System.out.println("b is greater");
+		
+else
+		
+System.out.println("c is greater");
+		
+		
+	
+}
+
+}
