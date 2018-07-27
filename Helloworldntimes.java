6230f80ca816e260e7986fea2a986d8336d45697
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 09:44:38 +0530

    Helloworldntimes.java

---
+++ Helloworldntimes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+import java.lang.*;
+				
+import java.io.*;
+				
+				
+/* Name of the class has to be "Main" only if the class is public. */
+				
+class Helloworldntimes				
+{
+					
+public static void main (String[] args) throws java.lang.Exception
+			
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter n value");
+n=sc.nextInt();
+for(int i=0;i<n;i++)
+System.out.println("Hello world"+i);
+}
+}
