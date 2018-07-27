2df5e0ed2c385b566a702be665e41b1826e21924
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 22:49:07 +0530

    PrimeNumb.java

---
+++ PrimeNumb.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PrimeNumb
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int low=20,high=50,i,flag=0;
+		while(low<high)
+		{
+			flag=0;
+			for(i=2;i<=low/2;++i)
+			{
+				if(low%i==0)
+				{
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)
+			{
+				System.out.println("numbers are"+low);
+			}
+			++low;
+		}
+	}
+}
