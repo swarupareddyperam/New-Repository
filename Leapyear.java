d96eaf17c89a0373ed4d040069db8d3cce530800
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 09:37:40 +0530

    Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Leapyear{
+	
+public static void main (String[] args) throws java.lang.Exception
+
+{
+int year;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter any year");
+year=sc.nextInt();
+if((year%400==0)||(year%4==0)||(year%100==0))
+System.out.println("Leap year");
+else
+System.out.println("Not a Leap year");
+}
+}
