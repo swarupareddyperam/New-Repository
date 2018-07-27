c85838a0b5abe510fa3712912db25f9e15bcef4d
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 12:16:02 +0530

    Count.java

---
+++ Count.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+  int n=998766,count==0;
+  while(n!=0)
+  {
+  n=n/10;
+  count++;
+  }
+  System.out.println("Number of digits"+count);
+	   
+	}
+}
