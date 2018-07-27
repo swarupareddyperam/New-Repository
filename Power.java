5454996ce9cbfab7790ca095324fa1c9e9182304
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-27 13:03:49 +0530

    Power

---
+++ Power.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int e=2,b=7;
+		 int p=(int)Math.pow(b,e);
+		 System.out.println(p);
+		
+	}
+}
