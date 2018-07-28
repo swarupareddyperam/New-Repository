2f6db0655d594d63c36641fa4a3ea41468f32d3b
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-28 08:15:06 +0530

    Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n,sum=0,temp,r;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter n value");
+	n=sc.nextInt();
+	temp=n;
+	
+	while(n>0)
+	{
+		r=n%10;
+		n=n/10;
+		sum=sum+(r*r*r);
+	}
+	if(temp==sum)
+	{
+		System.out.println("armstrong");
+	}
+	else
+	{
+			System.out.println(" not armstrong");
+		
+	}
+	
+	}
+}
