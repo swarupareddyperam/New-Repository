27fbfd3d3580fe02b0f5902a03454f208d943d65
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-28 09:46:09 +0530

    Multiplication_Table.java

---
+++ Multiplication_Table.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Multiplication_Table
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=19;
+		for(int i=1;i<5;i++)
+		{
+			System.out.println(n+"*"+i+"="+19*i);
+		}
+	}
+}
