80b925ac3a0f156c6817a94215eb118010caac54
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-28 09:38:23 +0530

    Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Factorial
+{
+	 static int factorial(int n)
+	{
+		if(n==0)
+		{
+			return 1;
+		}
+		else
+		{
+			return(n*factorial(n-1));
+		}
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int fact=1;
+		fact=factorial(5);
+		System.out.println(fact);
+		
+		
+			}
+}
