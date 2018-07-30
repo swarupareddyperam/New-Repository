5a9ee2316e9149496105f663ad6eda032b80171f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 18:30:57 +0530

    Helloworldntimes.java

--- Helloworldntimes.java
+++ Helloworldntimes.java
@@ -15,9 +15,9 @@ public static void main (String[] args) throws java.lang.Exception
 {
 int n;
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter n value");
+//System.out.println("Enter n value");
 n=sc.nextInt();
 for(int i=0;i<n;i++)
-System.out.println("Hello world"+i);
+       System.out.println("Hello");
 }
 }
