2cb47a778da2b2c9c5bf8f229b903cb684e40ae0
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 19:35:50 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -9,14 +9,17 @@ class Odd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int low=10,high=40,i,flag=0;
+		int low,high,i,flag=0;
+		Scanner sc=new Scanner(System.in);
+		low=sc.nextInt();
+		high=sc.nextInt();
 		while(low<high)
 		{
 			flag=0;
 			if(low%2!=0)
 			{
 				flag=0;
-				System.out.println("numbers are"+low);
+				System.out.println(" "+low);
 				
 			}
 			++low;
