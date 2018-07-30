2d88975c47b4e89b9482e955553a98ccd21f7096
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 20:20:50 +0530

    Update ArmstrongNumberWithinRange.java

--- ArmstrongNumberWithinRange.java
+++ ArmstrongNumberWithinRange.java
@@ -1,30 +1,55 @@
-import java.util.Scanner; 
-public class ArmstrongNumberWithinRange 
-{ 
-               public static void main(String args[]) 
-            { 
-               Scanner s=new Scanner(System.in); 
-               System.out.print("Enter the Range :"); 
-               int range=s.nextInt(); 
-               int a,b,c,sum,count=0; 
-               b=1; 
-               while(b<=range) 
-                    { 
-                        sum=0; 
-                        c=b; 
-                        while(c>0) 
-                             { 
-                                a=c%10; 
-                                sum=sum+(a*a*a); 
-                                c=c/10; 
-                             } 
-                         if(sum==b) 
-                       { 
-                         System.out.println(b+" is a Armstrong Number"); 
-                         count=count+1; 
-                       } 
-                          b++; 
-                     } 
-System.out.println("Total Armstrong Number Present With in that Range is "+count); 
-                } 
-} 
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		//public class Armstrong {
+
+    //public static void main(String[] args) {
+
+        int low, high;
+        Scanner sc=new Scanner(System.in);
+		low=sc.nextInt();
+		high=sc.nextInt();
+		
+
+        for(int number = low + 1; number < high; ++number) {
+
+            if (checkArmstrong(number))
+                System.out.print(number + " ");
+        }
+    }
+
+    public static boolean checkArmstrong(int num) {
+        int digits = 0;
+        int result = 0;
+        int originalNumber = num;
+
+        // number of digits calculation
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++digits;
+        }
+
+        originalNumber = num;
+
+        // result contains sum of nth power of its digits
+        while (originalNumber != 0) {
+            int remainder = originalNumber % 10;
+            result += Math.pow(remainder, digits);
+            originalNumber /= 10;
+        }
+
+        if (result == num)
+            return true;
+
+        return false;
+    }
+}
