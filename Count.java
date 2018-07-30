251e5f6962a97e97e6cd7a1684416372ec1e3431
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 18:48:03 +0530

    Count.java

--- Count.java
+++ Count.java
@@ -9,13 +9,15 @@ class Count
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-  int n=998766,count=0;
+  int n,count=0;
+  Scanner sc=new Scanner(System.in);
+  n=sc.nextInt();		
   while(n!=0)
   {
   n=n/10;
   count++;
   }
-  System.out.println("Number of digits"+count);
+  System.out.println(count);
 	   
 	}
 }
