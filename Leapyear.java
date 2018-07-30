b2d5c82b487c28c5e7f7c42b0449d79dc343f6de
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 18:28:08 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -13,11 +13,11 @@ public static void main (String[] args) throws java.lang.Exception
 {
 int year;
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter any year");
+//System.out.println("Enter any year");
 year=sc.nextInt();
 if((year%400==0)||(year%4==0)||(year%100==0))
-System.out.println("Leap year");
+    System.out.println("yes");
 else
-System.out.println("Not a Leap year");
+    System.out.println("no");
 }
 }
