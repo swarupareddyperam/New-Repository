76ce03a48c84cd5bd1e8fe2fe8c1d827181e03e5
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-30 18:14:32 +0530

    Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -12,12 +12,14 @@ class Vowel
 	{
 		int i=0;
 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a Alphabet");
+	//System.out.println("Enter a Alphabet");
 	char ch=sc.next().charAt(0);
 	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-	System.out.println("Enterd a Alphabet is vowel");
-	else
-	System.out.println("Entered a Alphabet is constant");
+	        System.out.println("Vowel");
+	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+	        System.out.println("Constant");
+        else
+		System.out.println("invalid");
      
 	}
 }
