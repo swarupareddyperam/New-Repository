71616177d24713aa236918d0bd4e91d47db269f0
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-31 12:54:42 +0530

    Update SortingQuick.java

--- SortingQuick.java
+++ SortingQuick.java
@@ -1,41 +1,67 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 import java.util.Scanner;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class SortingQuick
+ 
+/** Class QuickSort **/
+class SortingQuick 
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		//int a[];
-		int i,j,temp,n;
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		int a[]=new int[n];
-		for(i=0;i<n;i++)
-		{
-		
-			a[i]=sc.nextInt();
-		}
-		        for (i = 0; i < n - 1; i++) {
-
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]>a[j])
-			{
-				temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			}
-		}
-		        }
-		for(i=0;i<n;i++)
-		{
-			System.out.print(a[i]+" ");
-		}
-	}
+    /** Quick Sort function **/
+    public static void sort(int[] arr)
+    {
+        quickSort(arr, 0, arr.length - 1);
+    }
+    /** Quick sort function **/
+    public static void quickSort(int arr[], int low, int high) 
+    {
+        int i = low, j = high;
+        int temp;
+        int pivot = arr[(low + high) / 2];
+ 
+        /** partition **/
+        while (i <= j) 
+        {
+            while (arr[i] < pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
+            if (i <= j) 
+            {
+                /** swap **/
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+ 
+                i++;
+                j--;
+            }
+        }
+ 
+        /** recursively sort lower half **/
+        if (low < j)
+            quickSort(arr, low, j);
+        /** recursively sort upper half **/
+        if (i < high)
+            quickSort(arr, i, high);
+    }
+    /** Main method **/
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner( System.in );        
+        //System.out.println("Quick Sort Test\n");
+        int n, i;
+        /** Accept number of elements **/
+        //System.out.println("Enter number of integer elements");
+        n = scan.nextInt();
+        /** Create array of n elements **/
+        int arr[] = new int[ n ];
+        /** Accept elements **/
+        //System.out.println("\nEnter "+ n +" integer elements");
+        for (i = 0; i < n; i++)
+            arr[i] = scan.nextInt();
+        /** Call method sort **/
+        sort(arr);
+        /** Print sorted Array **/
+        //System.out.println("\nElements after sorting ");        
+        for (i = 0; i < n; i++)
+            System.out.print(arr[i]+" ");            
+       // System.out.println();            
+    }    
 }
