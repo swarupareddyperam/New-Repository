dca01ae6dd241bff168debd299eebbae2f58338b
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-31 16:34:38 +0530

    Rev.java

---
+++ Reve.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class Reve
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter string to reverse:");
+		
+		Scanner read = new Scanner(System.in);
+		String str = read.nextLine();
+		
+		StringBuilder sb = new StringBuilder(str);
+		
+		System.out.println("Reversed string is:");
+		System.out.println(sb.reverse().toString());
+	}
+}
