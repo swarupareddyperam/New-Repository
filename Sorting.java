c51e10e9145464825d98869a52a3ed1cbe5ab29f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-07-31 10:23:21 +0530

    Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,45 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Sorting
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		//int a[];
+		int i,j,temp,n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+		
+			a[i]=sc.nextInt();
+		}
+		        for (i = 0; i < n - 1; i++) {
+
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+		        }
+		for(i=0;i<n;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		
+		
+		
+		
+	}
+}
