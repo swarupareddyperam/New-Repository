c74803e700c481c818f8e8bb1c70b75f07f44c63
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-01 14:38:09 +0530

    Hellontimes.java

--- Hellontimes.java
+++ Hellontimes.java
@@ -7,7 +7,7 @@ import java.io.*;
 				
 /* Name of the class has to be "Main" only if the class is public. */
 				
-class Helloworldntimes				
+class Hellontimes				
 {
 					
 public static void main (String[] args) throws java.lang.Exception
