2acd4a5a0c5fddb2ed968694918e43283b974ca3
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-01 15:18:11 +0530

    Update PrimeNumb.java

--- PrimeNumb.java
+++ PrimeNumb.java
@@ -4,30 +4,23 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class PrimeNumb
+/* Name of the class has to be "Main" only f the class is public. */
+class Odd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int low,high,i,flag=0;
-		//int low,high,i,flag=0;
 		Scanner sc=new Scanner(System.in);
 		low=sc.nextInt();
 		high=sc.nextInt();
 		while(low<high)
 		{
 			flag=0;
-			for(i=2;i<=low/2;++i)
+			if(low%2=0)
 			{
-				if(low%i==0)
-				{
-					flag=1;
-					break;
-				}
-			}
-			if(flag==0)
-			{
-				System.out.println(" "+low);
+				flag=0;
+				System.out.print(" "+low);
+				
 			}
 			++low;
 		}
